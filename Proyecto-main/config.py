03f4187e94189b1ed744b40e2f66e13f9bb55510
Gonza xd <131913368+Maple-uu@users.noreply.github.com>
2024-10-22 20:49:41 -0300

    Delete Proyecto-main directory

--- Proyecto-main/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)  
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Canela17_@localhost:3306/search'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
