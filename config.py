83d25b4b3a7ea8e84e275fa65f1098a7d7f4e6e8
Ignaciojeda <ig.ojedao@duocuc.cl>
2024-11-04 16:16:19 -0300

    css

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)  
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/search'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Ojedaignacio01@localhost:3306/search'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
