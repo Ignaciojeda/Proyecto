01049a3b0076ccb357fc3ffd6877c538fe973014
Ignaciojeda <ig.ojedao@duocuc.cl>
2025-05-05 20:48:36 -0400

    Cambios en el config

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = os.urandom(24)  
+    SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = (
-        'mysql+pymysql://umioqejykf1rosez:'
-        'AwX7vAF5qCJbQBurc4Mp'
-        '@bberwxnczvkakxyarepa-mysql.services.clever-cloud.com:3306/'
-        'bberwxnczvkakxyarepa'
+        'mysql+pymysql://ucuut7lyq0o6mltn:'
+        'ZYquvLhikMUiw1liSVKY'
+        '@bp0xjhrmff7pogi61dt0-mysql.services.clever-cloud.com:3306/'
+        'bp0xjhrmff7pogi61dt0'
     )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
